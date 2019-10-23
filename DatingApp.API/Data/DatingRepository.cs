f1b33301809580e5796486235a27ba53d311f4ba
unknown <info4you2013@gmail.com>
2019-10-24 00:00:05 +0100

    End Of section 7

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _context;
+        public DatingRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        void IDatingRepository.Add<T>(T entity)
+        {
+            _context.Add(entity);
+        }
+
+       void IDatingRepository.Delete<T>(T entity)
+        {
+            _context.Remove(entity);
+        }
+
+        Task<User> IDatingRepository.GetUser(int id)
+        {
+            var User = _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+
+            return User;
+        }
+
+        async Task<IEnumerable<User>> IDatingRepository.GetUsers()
+        {
+            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+            return users;
+        }
+
+        async Task<bool> IDatingRepository.SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
